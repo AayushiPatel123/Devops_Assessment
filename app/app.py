ff097ef49e600d71296401889c7b6ba95f2532c6
AayushiPatel123 <patel9i5@uwindsor.ca>
2024-02-26 02:24:57 -0500

    fix: resolved lint issues in app.py

--- app/app.py
+++ app/app.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
+
 @app.route('/')
 def home():
     return "Welcome to the Flask Web Application!"
+
 
 @app.route('/api/data', methods=['GET', 'POST'])
 def data():
@@ -19,14 +21,18 @@ def data():
         data = request.json
         return jsonify({"received_data": data}), 201
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return jsonify(error=str(e)), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
     return jsonify(error=str(e)), 500
 
-if __name__ == '_main_':
+
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
